c63bee0e1e97463c1c3f71f0e2560cb8f8841aae
Andreas Wiedholz <andreas.wiedholz@xitaso.com>
2021-03-11 10:37:44 +0100

    initial commit

---
+++ definitions.h
@@ -0,0 +1,12 @@
+#ifndef DEFINITIONS_H
+#define DEFINITIONS_H
+
+#define GRASP_SUCCESS   1
+#define GRASP_FAILURE   0
+#define GRASP_ABORT     -1
+
+#define USED_ROBOT_ENV "USED_ROBOT"
+#define USED_ROBOT_YOUBOT "youbot"
+#define USED_ROBOT_YOUBOT_SOFT "youbot_soft"
+
+#endif /*DEFINITIONS_H*/
